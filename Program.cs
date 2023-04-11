fd78f749c03458783a3d8d3430c03b88c8078bb2
Егор Сивцев <gumben7@gmail.com>
2023-04-11 08:52:12 +0700

    pre

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿namespace TimeSpace {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var t1 = new Time(3600);
+            var t2 = new Time();
+            t2.ChangeTimeByHours(1);
+            Console.WriteLine(t1.GetHashCode() == t2.GetHashCode());
+            Console.WriteLine(t1.Equals(t2));
+            Console.WriteLine(t1 == t2);
         }
     }
 }
